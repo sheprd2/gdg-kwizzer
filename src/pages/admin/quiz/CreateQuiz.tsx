4f60369f8340b1318ae53c64d55aa6c073df3261
mathew2103 <mathew2103@gmail.com>
2025-12-18 14:59:26 +0530

    fixes

--- src/pages/admin/quiz/CreateQuiz.tsx
+++ src/pages/admin/quiz/CreateQuiz.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const CreateQuiz = () => {
   return (
     <div>CreateQuiz</div>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Routes, Route, Navigate } from 'react-router'
 import Dashboard from '../pages/admin/dashboard'
 import App from '../App'
